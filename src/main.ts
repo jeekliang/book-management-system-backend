d8d499d7fb6e3146161339854991d6818cf3cd25
jxliang <jeekliang@163.com>
2024-11-08 15:44:20 +0800

    feat: 增加swagger配置

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
@@ -10,11 +11,21 @@ import { TransformInterceptor } from './interceptor/transform.interception';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  // DTO 验证拦截器
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useStaticAssets(join(__dirname, '../uploads'), { prefix: '/uploads' });
   // app.useLogger(app.get(WINSTON_LOGGER_TOKEN));
-  
+
+  // const config = new DocumentBuilder()
+  //   .setTitle('Test example')
+  //   .setDescription('The API description')
+  //   .setVersion('1.0')
+  //   .addTag('test')
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('doc', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
